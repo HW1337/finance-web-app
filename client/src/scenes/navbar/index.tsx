caf94de89a5240012d30d0faac870ae4c068a378
HW1337 <kskobelin@ya.ru>
2023-11-06 05:41:16 +0700

    added FlexBetween component

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,14 +1,18 @@
-import { UseState } from 'react';
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 import { Box, Typography, useTheme} from "@mui/material";
+import FlexBetween from "@/components/FlexBetween"
 
 type Props = {}
 
 const Navbar = (props: Props) => {
     const { palette } = useTheme();
   return (
-    <div>Navbar</div>
-  )
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+     
+    </FlexBetween>
+     )
+  
 }
 
 export default Navbar;
