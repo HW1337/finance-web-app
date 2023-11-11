165fbc73fe715eca67b05c5eac4d1230ce764dfb
HW1337 <kskobelin@ya.ru>
2023-11-11 17:41:41 +0700

    Revert "Merge pull request #27 from HW1337/feature"
    This reverts commit b4c3aaff79ca6f82067d4792a3bc8efbb007b730, reversing
    changes made to f771346a55e090ae438f137bda2c76d96bb4d427.

--- server/index.js
+++ server/index.js
@@ -5,9 +5,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import kpiRoutes from "./routes/kpi.js" 
-import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
 
 // CONFIGURATIONS
 dotenv.config();
@@ -20,9 +17,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-/* ROUTES */
-app.use("/kpi", kpiRoutes);
-
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 9000;
 mongoose
@@ -32,8 +26,9 @@ mongoose
   })
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-    //await mongoose.connection.db.dropDatabase();
-    //KPI.insertMany(kpis);
+
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
     // Product.insertMany(products);
     // Transaction.insertMany(transactions);
   })
